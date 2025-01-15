bce1a5f90b0ad2b2ab3d4bf90bd2061b05eda805
Smart Dev <TimBeamon11910@gmail.com>
2025-01-14 22:29:05 -0500

    Update faqs.jsx

--- src/components/views/faqs.jsx
+++ src/components/views/faqs.jsx
@@ -41,36 +41,44 @@ const Faqs = () => {
                     </p>
                   </li>
                   <li>
-                    <a></a>
-                    <p></p>
+                    <a>How Is the Price of AR+ Token Determined?</a>
+                    <p>The initial sale price will be $0.50, and the second sale price will be $0.75. Afterward, the token will be listed on exchanges at $1.00 and will be subject to market conditions.</p>
                   </li>
                   <li>
-                    <a></a>
-                    <p></p>
+                    <a>How Can I Store AR+ Token? </a>
+                    <p>After the pre-sale, AR+ Token will aim to list on exchanges. However, the success of the listing depends on the level of community participation during the pre-sale, as exchanges prioritize projects with high engagement. Our primary goal is to list on a centralized exchange (CEX). If a CEX listing is not achieved, we will ensure listing on a decentralized exchange (DEX) as a minimum. Future listings will follow our roadmap accordingly. </p>
                   </li>
                   <li>
-                    <a></a>
-                    <p></p>
+                    <a>How Can I Use AR+ Token?</a>
+                    <p>You can spend your tokens on services offered by the platform, transfer them to other users, or trade them on exchanges.  
+                    </p>
                   </li>
                   <li>
-                    <a></a>
-                    <p></p>
+                    <a>What Is the Economic Model of AR+ Token?</a>
+                    <p>The total supply is 500 billion tokens, with 99 billion tokens in circulation. Additionally, 13 billion tokens will be burned annually.  
+                    </p>
                   </li>
                   <li>
-                    <a></a>
-                    <p></p>
+                    <a>Is AR+ Token Secure?  
+                    </a>
+                    <p>Yes, AR+ Token benefits from the security features of blockchain technology as it operates on the Polygon network.  
+                    </p>
                   </li>
                   <li>
-                    <a></a>
-                    <p></p>
+                    <a>Can I Earn Rewards on the Platform? </a>
+                    <p>Yes, you can earn rewards through your activities on the platform, which will be distributed as AR+ Tokens.</p>
                   </li>
                   <li>
-                    <a></a>
-                    <p></p>
+                    <a>Which Cryptocurrencies Can I Trade AR+ Token With?</a>
+                    <p>You can trade AR+ Token primarily with MATIC, USDT, and other cryptocurrencies supported on the Polygon network.</p>
                   </li>
                   <li>
-                    <a></a>
-                    <p></p>
+                    <a>How Will the Taxes Collected from AR+ Token Transactions Be Used?</a>
+                    <p>98% of the collected taxes will be used for buybacks to maintain and increase the value of AR+ Token. The remaining 2% will be allocated to social responsibility projects.</p>
+                  </li>
+                  <li>
+                    <a>On Which Exchanges Will AR+ Token Be Listed?</a>
+                    <p>After the pre-sale, AR+ Token will aim to list on exchanges. However, the success of the listing depends on the level of community participation during the pre-sale, as exchanges prioritize projects with high engagement. Our primary goal is to list on a centralized exchange (CEX). If a CEX listing is not achieved, we will ensure listing on a decentralized exchange (DEX) as a minimum. Future listings will follow our roadmap accordingly.  </p>
                   </li>
                 </ul>
               </div>
