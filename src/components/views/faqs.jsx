8a5e706fa4258d367b2de9a29390dee4addc0c74
Smart Dev <TimBeamon11910@gmail.com>
2025-01-14 22:41:55 -0500

    Update faqs.jsx

--- src/components/views/faqs.jsx
+++ src/components/views/faqs.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Faqs = () => {
   return (
-    <div className="faq-section style-five pt-100 pb-100">
+    <div className="faq-section style-five pt-100 pb-100" style={{ height: "100%"}}>
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
