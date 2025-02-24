ca4e470a970774c1148f54a559df5236b3166b7d
Smart Dev <TimBeamon11910@gmail.com>
2025-02-24 13:58:10 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('vue')) {
+              return 'vendor-framework'; // Separate React/Vue
+            }
+            return 'vendor'; // Separate other libraries
+          }
+        },
+      },
+    },
+  },
 })
