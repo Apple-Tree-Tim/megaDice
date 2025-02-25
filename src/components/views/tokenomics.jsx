4ee2d8fff26498f62059b0749570e9040dd6517a
Smart Dev <TimBeamon11910@gmail.com>
2025-02-25 09:23:41 -0500

    Update tokenomics.jsx

--- src/components/views/tokenomics.jsx
+++ src/components/views/tokenomics.jsx
@@ -96,62 +96,9 @@ const Tokenomics = ({stage}) => {
                 <h2>$AR+ has total supply of <span>500,000,000,000</span></h2>
                 <p>The AR PLUS token serves as the utility token of our platform, enabling users to access premium content, participate in community governance, and redeem exclusive rewards and benefits.</p>
               </div>
-              <table id="example" className="table table-striped table-bordered">
-                <thead>
-                  <tr>
-                    <th id="all_token" style={{ textAlign: "left", fontWeight: "bold", color: "#fff" }}>All Tokens</th>
-                    <th id="gainers" style={{ textAlign: "left", fontWeight: "bold", color: "#fff" }}>Gainers</th>
-                    <th id="date" style={{ textAlign: "left", fontWeight: "bold", color: "#fff" }}>Total Supply</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr className="single">
-                    <td> <img src="assets/images/new/01.png" alt="" /> Bitcoin <span>BTC</span></td>
-                    <td><img src="assets/images/new/0.png" alt="" /> 5.43%</td>
-                    <td>21M</td>
-                  </tr>
-                  <tr className="one">
-                    <td> <img src="assets/images/new/2.png" alt="" /> Ethereum <span>ETH</span></td>
-                    <td><img src="assets/images/new/3.png" alt="" /> 0.23%</td>
-                    <td>0.5M</td>
-                  </tr>
-                  <tr className="double">
-                    <td> <img src="assets/images/new/4.png" alt="" /> Tether <span>THR</span></td>
-                    <td><img src="assets/images/new/0.png" alt="" /> 6.96%</td>
-                    <td>36M</td>
-                  </tr>
-                  <tr className="one">
-                    <td> <img src="assets/images/new/5.png" alt="" /> Binance Coin <span>BNC</span></td>
-                    <td><img src="assets/images/new/3.png" alt="" /> 1.90%</td>
-                    <td>1.9M</td>
-                  </tr>
-                  <tr className="triple">
-                    <td> <img src="assets/images/new/4.png" alt="" /> Solana <span>SOL</span></td>
-                    <td><img src="assets/images/new/0.png" alt="" /> 5.43%</td>
-                    <td>27M</td>
-                  </tr>
-                  <tr className="one">
-                    <td> <img src="assets/images/new/5.png" alt="" /> Binance Coin <span>BNC</span></td>
-                    <td><img src="assets/images/new/3.png" alt="" /> 1.90%</td>
-                    <td>1.9M</td>
-                  </tr>
-                  <tr className="triple">
-                    <td> <img src="assets/images/new/8.png" alt="" /> Dogecoin<span>DOGE</span></td>
-                    <td><img src="assets/images/new/0.png" alt="" /> 5.43%</td>
-                    <td>23M</td>
-                  </tr>
-                  <tr className="one">
-                    <td> <img src="assets/images/new/01.png" alt="" /> Bitcoin <span>BTC</span></td>
-                    <td><img src="assets/images/new/3.png" alt="" /> 5.43%</td>
-                    <td>21M</td>
-                  </tr>
-                  <tr className="two">
-                    <td> <img src="assets/images/new/2.png" alt="" /> Bitcoin <span>BTC</span></td>
-                    <td><img src="assets/images/new/0.png" alt="" /> 5.43%</td>
-                    <td>21M</td>
-                  </tr>
-                </tbody>
-              </table>
+              <div>
+                <img src="assets/images/main.png" width="100%" alt=""  style={{marginTop: "50px"}}/>
+              </div>
             </div>
           </div>
         </div>
